027ae9aad7e180ab199ac3dcdc01114702f4ccb7
Mily Gardiol <gardiol72@gmail.com>
2022-12-02 21:27:12 -0300

    Websockets

---
+++ Clase6-Websockets.js
@@ -0,0 +1,12 @@
+
+//WEBSOCKETS
+//Protocolo de red basado en TCP
+//TCP=> establece conecciones entre dos sockets(puntos finales de comunicación || cliente = servidor)
+//contiene conección bidireccional luego del handshake
+
+
+//para no hacer refresh en la página, se emplea el servidor en tiempo de real con la conección con los sockets, se usa en los chats, juegos, y manejo de datos actualizados en el momento.
+
+//JAVASCRIPT => socket.io
+
+
