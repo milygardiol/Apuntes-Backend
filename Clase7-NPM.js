7c01d6bed86f3e3b6bf09842d56576f35ab16b93
Mily Gardiol <gardiol72@gmail.com>
2022-12-02 21:27:59 -0300

    NPM como herramienta de dev

---
+++ Clase7-NPM.js
@@ -0,0 +1,12 @@
+// lenguajes programación:
+// bajo nivel => computadora (cambio de magnetismo en las ranuras de la memoria)
+// alto nivel => abstracción (persona)
+
+/* 
+TRANSPILADOR: tipo de compilador que traduce de un lenguaje a otro con diferencia de abstracción.
+COMPILADOR: software que traduce un programa escrito en un lenguaje de alto nivel en lenguaje máquina.
+*/
+
+//TYPESCRIPT
+//compilador => navegador
+//tipado estático, muestra los tipos de datos, resuelve problemas de JS
